94e8012038706613341724ee33fe7df1fb04bd41
Omkar Patil <omkar.s.patil-1@ou.edu>
2018-02-13 16:24:48 -0500

    Random Array Update

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     var pumps;
     var total = 0; // money that has been earned in total
     var rounds_played = 12;
-    var explode_array = [4, 20 ,40, 10, 6, 16, 20, 34, 31, 15, 40, 20];
+    var explode_array = Array.from({length: 20}, () => Math.floor(Math.random() * 40));
     var maximal_pumps = 186;
     var pumpmeup; // number pumps in a given round; is updated each round
     var number_pumps = []; // arrays for saving number of pumps
